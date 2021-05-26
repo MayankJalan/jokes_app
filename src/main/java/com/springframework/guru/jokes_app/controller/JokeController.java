1c98e37251b81196a884b8e69721b3c577b22bb5
Mayank Jalan <mnkjln780@gmail.com>
2021-05-27 00:34:39 +0530

    Added View for App

--- src/main/java/com/springframework/guru/jokes_app/controller/JokeController.java
+++ src/main/java/com/springframework/guru/jokes_app/controller/JokeController.java
@@ -6,20 +6,19 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class JokeController
-{
-   private final JokeService jokeService;
+public class JokeController {
+
+    private final JokeService jokeService;
 
     public JokeController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"/",""})
-        public String showJoke(Model model)
-        {
-            model.addAttribute("joke",jokeService.getJoke());
-            return "index";
-        }
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
 
+        model.addAttribute("joke", jokeService.getJoke());
 
+        return "index";
+    }
 }
